b7e313b5c023e9da2821339ab6cbb1c7f89975fc
mdanieladla <mariadanieladarnea@gmail.com>
2022-06-19 18:28:26 +0200

    added axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 // import { ReactDOM } from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  handleSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { images: [] };
+
+  handleSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term },
+    });
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.handleSearchSubmit} />
+        <span>Found: {this.state.images.length} images</span>
       </div>
     );
   }
