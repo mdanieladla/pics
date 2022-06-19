e1cbada33e7810361083ddd982c3315844e80b92
mdanieladla <mariadanieladarnea@gmail.com>
2022-06-19 13:19:18 +0200

    added handleSubmitForm and props inside handleSubmitForm

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,15 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
+  handleSubmitForm = (ev) => {
+    ev.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
       <div className='ui segment'>
-        <form className='ui form'>
+        <form className='ui form' onSubmit={this.handleSubmitForm}>
           <div className='field'>
             <label>Image Search</label>
             <input
