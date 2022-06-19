fbfaa74ca3702e8d9678afbdb3f86e40f86ec535
mdanieladla <mariadanieladarnea@gmail.com>
2022-06-19 13:20:19 +0200

    change app functional component for a class based component and added handleSearchSubmit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { ReactDOM } from 'react';
+// import { ReactDOM } from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className='ui container' style={{ marginTop: '10px' }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  handleSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div className='ui container' style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.handleSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
